fa57e86549b3c6523cc456979c3372151f7bbf5c
yo <chaheein@gmail.com>
2021-05-09 00:12:05 -0700

    Create InstallJenkinsScript.sh

---
+++ InstallJenkinsScript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yum update -y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum upgrade -y
+yum install java-1.8.0-openjdk-devel -y
+yum install jenkins -y
+systemctl daemon-reload
+systemctl start jenkins
