58966b30113600fab9b767c4d4e00c7019463cba
yo <chaheein@gmail.com>
2021-05-09 02:55:34 -0700

    Update InstallJenkins.sh

--- InstallJenkins.sh
+++ InstallJenkins.sh
@@ -2,6 +2,7 @@
 
 yum update -y
 yum install wget -y
+yum install maven -y
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum upgrade -y
